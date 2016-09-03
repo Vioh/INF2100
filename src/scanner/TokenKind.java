4f8d16365e9440d973e23b622d7fc6467e7fc09e
Vioh <syhd.ifi.uio.no>
2016-09-03 09:48:21 +0200

    plan.txt file added. Reformat prekode using tabs instead of space

---
+++ src/scanner/TokenKind.java
@@ -0,0 +1,86 @@
+package scanner;
+
+// Note that tokens found in standard Pascal but not in Pascal2016
+// have been commented out.
+
+public enum TokenKind {
+	nameToken("name"),
+	intValToken("number"),
+	charValToken("char"),
+	
+	addToken("+"),
+	assignToken(":="),
+	colonToken(":"),
+	commaToken(","),
+	dotToken("."),
+	equalToken("="),
+	greaterToken(">"),
+	greaterEqualToken(">="),
+	leftBracketToken("["),
+	leftParToken("("),
+	lessToken("<"),
+	lessEqualToken("<="),
+	multiplyToken("*"),
+	notEqualToken("<>"),
+	rangeToken(".."),
+	rightBracketToken("]"),
+	rightParToken(")"),
+	semicolonToken(";"),
+	subtractToken("-"),
+
+	andToken("and"), 
+	arrayToken("array"),
+	beginToken("begin"), 
+	constToken("const"),
+	divToken("div"), 
+	doToken("do"), 
+	elseToken("else"), 
+	endToken("end"),
+	functionToken("function"),
+	ifToken("if"), 
+	modToken("mod"),
+	notToken("not"),
+	ofToken("of"), 
+	orToken("or"),
+	procedureToken("procedure"), 
+	programToken("program"),
+	thenToken("then"), 
+	varToken("var"),
+	whileToken("while"),
+	eofToken("e-o-f");
+
+	private String image;
+
+	TokenKind(String im) {
+		image = im;
+	}
+
+
+	public String identify() {
+		return image + " token";
+	}
+
+	@Override public String toString() {
+		return image;
+	}
+
+
+	public boolean isFactorOpr() {
+		return this==multiplyToken || this==divToken ||
+			this==modToken || this==andToken;
+	}
+
+	public boolean isPrefixOpr() {
+		return this==addToken || this==subtractToken;
+	}
+
+	public boolean isRelOpr() {
+		return this==equalToken || this==notEqualToken ||
+			this==lessToken || this==lessEqualToken ||
+			this==greaterToken || this==greaterEqualToken;
+	}
+
+	public boolean isTermOpr() {
+		return isPrefixOpr() || this==orToken;
+	}
+}
