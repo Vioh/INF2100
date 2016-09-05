403fde29ed60b5f861fa369708a0ec5313b147b1
Vioh <syhd.ifi.uio.no>
2016-09-05 21:50:36 +0200

    Scanner.java completed, compiled and working!

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -1,8 +1,6 @@
 package scanner;
-
 import main.Main;
 import static scanner.TokenKind.*;
-
 import java.io.*;
 
 public class Scanner {
@@ -10,6 +8,47 @@ public class Scanner {
 	private LineNumberReader srcFile = null;
 	private String srcFileName, srcLine = "";
 	private int srcPos = 0;
+	
+	public Scanner(String fileName) {
+		srcFileName = fileName;
+		try {
+			srcFile = new LineNumberReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			Main.error("Cannot read " + fileName + "!");
+		}
+		readNextToken(); 
+		readNextToken();
+	}
+	
+	public String identify() {
+		return "Scanner reading " + srcFileName;
+	}
+
+	public int curLineNum() {
+		return curToken.lineNum;
+	}
+	
+	private int getFileLineNum() {
+		return (srcFile!=null ? srcFile.getLineNumber() : 0);
+	}
+	
+	private boolean isLetterAZ(char c) {
+		return 'A'<=c && c<='Z' || 'a'<=c && c<='z';
+	}
+	
+	private boolean isDigit(char c) {
+		return '0'<=c && c<='9';
+	}
+	
+	private boolean isSpace(char c) {
+		return c == ' ' || c == '\t';
+	}
+	
+	private void error(String message) {
+		Main.error("Scanner error on " +
+		           (curLineNum()<0 ? "last line" : "line "+curLineNum()) + 
+		           ": " + message);
+	}
 	
 	/* Parser tests:
 	public void test(TokenKind t) {
@@ -29,82 +68,6 @@ public class Scanner {
 	}
 	*/
 	
-	public Scanner(String fileName) {
-		srcFileName = fileName;
-		try {
-			srcFile = new LineNumberReader(new FileReader(fileName));
-		} catch (FileNotFoundException e) {
-			Main.error("Cannot read " + fileName + "!");
-		}
-		readNextToken(); readNextToken();
-	}
-	
-	public String identify() {
-		return "Scanner reading " + srcFileName;
-	}
-
-	public int curLineNum() {
-		return curToken.lineNum;
-	}
-	
-	private int getFileLineNum() {
-		return (srcFile!=null ? srcFile.getLineNumber() : 0);
-	}
-
-	private void error(String message) {
-		Main.error("Scanner error on " +
-		           (curLineNum()<0 ? "last line" : "line "+curLineNum()) + 
-		           ": " + message);
-	}
-
-	
-	public void readNextToken() {
-		if(srcFile == null) return; //-----------------maybe give an error?
-		curToken = nextToken; 
-		nextToken = null;
-		int k;
-		char c;
- 
-		// SKIP OVER WHITE SPACES, HANDLE EOF-TOKEN IF NEEDED
-		while(true) {
-			if(srcPos >= srcLine.length()) {
-				readNextLine();
-				if(srcFile == null) {
-					nextToken = new Token(TokenKind.eofToken,0);
-					Main.log.noteToken(nextToken); return;
-				}
-			}
-			if(! isSpace(srcLine.charAt(srcPos))) break;
-		}
-		// SKIP OVER COMMENTS
-		if(srcLine.charAt(srcPos) == '{') {
-			handleComment(true); //bracket-style comment
-		} else if(srcLine.substring(srcPos,srcPos+1).equals("/*") {
-			handleComment(false); //C-style comment
-		} 
-		// MAIN SWITCH TO GET NEXT TOKEN
-		if(isDigit(srcLine.charAt(srcPos))) {
-			for(k = srcPos+1; k < srcLine.length(); k++) {
-				c = srcLine.charAt(k);
-				if(!isDigit(c)) break;
-			}
-			int num = Integer.parseInt(srcLine.substring(srcPos, k));
-			nextToken = new Token(num, curLineNum());
-		}
-		else if(isLetterAZ(srcLine.charAt(srcPos))) {
-			for(k = srcPos+1; k < srcLine.length(); k++) {
-				c = srcLine.charAt(k);
-				if(!isLetterAZ(c) && !isDigit(c)) break;
-			}
-			String str = srcLine.substring(srcPos, k);
-			nextToken = new Token(str, curLineNum());
-		}
-		else {
-			handleSymbol();
-		}
-		Main.log.noteToken(nextToken);
-	}
-
 	private void readNextLine() {
 		if (srcFile != null) {
 			try {
@@ -123,24 +86,71 @@ public class Scanner {
 		if (srcFile != null) 
 			Main.log.noteSourceLine(getFileLineNum(), srcLine);
 	}
+	
+	
+	/** Reads the next token. Assumes that EOF-token has not been reached yet */
+	public void readNextToken() {
+		curToken = nextToken; 
+		nextToken = null;
+		char c;
+		int k;
 
-	private boolean isLetterAZ(char c) {
-		return 'A'<=c && c<='Z' || 'a'<=c && c<='z';
+		// HANDLE WHITE SPACES AND COMMENTS
+		while(true) {
+			// Skip over white spaces, and handle EOF-token if needed:
+			while(true) {
+				if(srcPos >= srcLine.length()) {
+					readNextLine();
+					if(srcFile == null) {
+						nextToken = new Token(TokenKind.eofToken,0);
+						Main.log.noteToken(nextToken); 
+						return;
+					}
+				} 
+				if(! isSpace(srcLine.charAt(srcPos))) break;
+				srcPos++;
+			}
+			// Skip over comment:
+			if(srcLine.charAt(srcPos) == '{') {
+				handleComment(true); //bracket-style comment
+			} else if(srcLine.substring(srcPos,srcPos+2).equals("/*")) {
+				handleComment(false); //C-style comment
+			}
+			// Stop the loop if there are no white spaces left: 
+			if(! isSpace(srcLine.charAt(srcPos))) break;
+		}
+
+		// MAIN SWITCH TO GET NEXT TOKEN
+		if(isDigit(srcLine.charAt(srcPos))) {
+			for(k = srcPos+1; k < srcLine.length(); k++) {
+				c = srcLine.charAt(k);
+				if(!isDigit(c)) break;
+			}
+			int num = Integer.parseInt(srcLine.substring(srcPos, k));
+			nextToken = new Token(num, srcFile.getLineNumber());
+			srcPos = k;
+		}
+		else if(isLetterAZ(srcLine.charAt(srcPos))) {
+			for(k = srcPos+1; k < srcLine.length(); k++) {
+				c = srcLine.charAt(k);
+				if(!isLetterAZ(c) && !isDigit(c)) break;
+			}
+			String str = srcLine.substring(srcPos, k);
+			nextToken = new Token(str, srcFile.getLineNumber());
+			srcPos = k;
+		}
+		else {
+			handleSymbol();
+		}
+		Main.log.noteToken(nextToken);
 	}
-	
-	private boolean isDigit(char c) {
-		return '0'<=c && c<='9';
-	}
-	
-	private boolean isSpace(char c) {
-		return c == ' ' || c == '\t';
-	}
+
 	
 	/** Processes symbols and create a token if there is a symbol token. */
 	private void handleSymbol() {
 		char c1 = srcLine.charAt(srcPos);
 		char c2 = srcLine.charAt(srcPos+1);
-		int cln = curLineNum();
+		int cln = srcFile.getLineNumber();
 		boolean isTwoCharToken = false;
 		
 		// MAIN SWITCH TO DETERMINE THE TYPE OF TOKEN
@@ -189,30 +199,32 @@ public class Scanner {
 		else srcPos++;
 	}
 	
+	
 	/** Processes a char literal. */
 	private void handleQuote() {
 		boolean isLegal = false;
+		int cln = srcFile.getLineNumber();
 		if(++srcPos + 2 < srcLine.length()) {
-			if(srcLine.substring(srcPos,srcPos+3).equals("'''") {
-				nextToken = new Token('\'', curLineNum());
+			if(srcLine.substring(srcPos,srcPos+3).equals("'''")) {
+				nextToken = new Token('\'', cln);
+				srcPos = srcPos + 3;
 				isLegal = true;
 			} else if(srcLine.charAt(srcPos+1) == '\'') {
-				nextToken = new Token(srcLine.charAt(srcPos), curLineNum());
+				nextToken = new Token(srcLine.charAt(srcPos), cln);
+				srcPos = srcPos + 2;
 				isLegal = true;
 			}
 		}
-		if(!isLegal) {
-			error("Illegal char literal!");
-		}
-		return isLegal;
+		if(!isLegal) error("Illegal char literal!");
 	}
+	
 	
 	/**
 	 * Skips over a comment.
 	 * @param isBracketComment true for curly-bracket style, false for C-style.
 	 */
 	private void handleComment(boolean isBracketComment) {
-		int cln = curLineNum();
+		int cln = srcFile.getLineNumber();
 		String errorMessage = "No end for comment starting on line " + cln;
 		
 		// SKIP OVER OPENING SYMBOLS
