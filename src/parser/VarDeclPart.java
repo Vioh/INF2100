737262badae44aa9506edeed91bec7925e452dae
emcuevas <migs_cuevas@yahoo.com>
2016-10-12 20:30:15 +0200

    changes 05

--- src/parser/VarDeclPart.java
+++ src/parser/VarDeclPart.java
@@ -4,5 +4,31 @@ import scanner.*;
 import static scanner.TokenKind.*;
 
 class VarDeclPart extends PascalSyntax {
+	ArrayList<VarDecl> vdlist = new ArrayList<VarDecl>();
 	
+	public VarDeclPart(int lNum) {
+		super(lNum);
+	}
+	
+	@Override
+	public String identify() {
+		return "<var-decl-part> on line " + lineNum;
+	}
+	//MIGZ
+	public static VarDeclPart parse(Scanner s) {
+		enterParser("var-decl-part");
+		
+		VarDeclPart vdp = new VarDeclPart(s.curLineNum());
+		
+		while(True) {
+			vdp.vdlist.add(VarDecl.parse(s));
+			if(s.test("\n")) { //MÅ FORANDRE FOR Å SJEKKE RIKTIG OM DET ER EN TIL VAR DECL
+				s.readNextToken();
+				continue;
+			} else break;
+		}
+		
+		leaveParser("var-decl-part");
+		return vdp;
+	}
 }
