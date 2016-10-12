9e16d49f2bf5c186608b53949542af0f880bbce6
emcuevas <migs_cuevas@yahoo.com>
2016-10-12 20:17:05 +0200

    changes 04

--- src/parser/VarDecl.java
+++ src/parser/VarDecl.java
@@ -4,5 +4,30 @@ import scanner.*;
 import static scanner.TokenKind.*;
 
 class VarDecl extends PascalDecl {
+	TypeDecl td;
+	String name;
+	
+	public VarDecl(int lNum) {
+		super(lNum);
+	}
+	//MIGZ
+	@Override
+	public String identify() {
+		return "<var-decl> on line " + lineNum;
+	}
+	
+	public static VarDecl parse(Scanner s) {
+		enterParser("var-decl");
+		
+		VarDecl vd = new VarDecl(s.curLineNum());
+		
+		this.name = s.curToken.id;
+		s.skip(colonToken);
+		vd.td = TypeDecl.parse(s);
+		s.skip(semicolonToken);
+	
+		leaveParser("var-decl");
+		return vd;
+	}
 	
 }
