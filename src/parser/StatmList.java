bbc21eff8c596d6e9fd46a2b1eb802e1b20b73c7
emcuevas <migs_cuevas@yahoo.com>
2016-10-12 20:09:21 +0200

    changes 03

--- src/parser/StatmList.java
+++ src/parser/StatmList.java
@@ -4,5 +4,32 @@ import scanner.*;
 import static scanner.TokenKind.*;
 
 class StatmList extends PascalSyntax {
+	Arraylist<Statement> stlist = new ArrayList<Statement>();
+	
+	public StatmList(int lNum) {
+		super(lNum);
+	}
+	
+	@Override
+	public String identify() {
+		return "<statm-list> on line " + lineNum;
+	}
+	
+	public static StatmList parse(Scanner s) {
+		enterParser("statm-list");
+		
+		StatmList stl = new StatmList(s.curLineNum());
+		
+		while(True) {
+			stl.stlist.add(Statement.parse(s));
+			if(s.test(semicolonToken)) {
+				s.readNextToken();  //skip semicolon
+				continue;
+			} else break;
+		}
+		
+		leaveParser("statm-list");
+		return stl;
+	}
 	
 }
