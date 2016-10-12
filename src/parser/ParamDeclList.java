f67265f70d13872d0adbe938ef602dd68a8b20ea
emcuevas <migs_cuevas@yahoo.com>
2016-10-12 20:00:49 +0200

    changes 02

--- src/parser/ParamDeclList.java
+++ src/parser/ParamDeclList.java
@@ -4,5 +4,33 @@ import scanner.*;
 import static scanner.TokenKind.*;
 
 class ParamDeclList extends PascalSyntax {
+	ArrayList<ParamDecl> pdlist = new ArrayList<ParamDecl>();
+	
+	public ParamDeclList(int lNum) {
+		super(lNum);
+	}
+	
+	@Override
+	public String identify() {
+		return "<param-decl-list> on line " + lineNum;
+	}
+	//MIGZ
+	public static ParamDeclList parse(Scanner s) {
+		enterParser("param-decl-list");
+		
+		ParamDeclList pdl = new ParamDeclList(s.curLineNum());
+		s.skip(leftParToken);
+		
+		while(True) {
+			pdl.pdlist.add(ParamDecl.parse(s));
+			if(s.test(semicolonToken)) {
+				s.readNextToken(); //skip semicolon
+				continue;
+			} else break; 
+		}
+		s.skip(rightParToken);
+		leaveParser("param-decl-list");
+		return pdl;
+	}
 	
 }
