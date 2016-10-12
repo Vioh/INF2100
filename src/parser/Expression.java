5ffd1ebd8020f5f7aa8851f9c4b9d3d961c76036
emcuevas <migs_cuevas@yahoo.com>
2016-10-12 19:09:09 +0200

    changes 01

--- src/parser/Expression.java
+++ src/parser/Expression.java
@@ -4,5 +4,32 @@ import scanner.*;
 import static scanner.TokenKind.*;
 
 class Expression extends PascalSyntax {
+	SimpleExpr se1;
+	SimpleExpr se2;
+	RelOperator ro;
 	
+	public Expression(int lNum) {
+		super(lNum);
+	}
+	
+	@Override
+	public String identify() {
+		return "<expression> on line " + lineNum;
+	}
+	
+	public static Expression parse(Scanner s) {
+		enterParser("expression");
+		
+		Expression e = new Expression(s.curLineNum());
+		
+		
+		e.se1 = SimpleExpr.parse(s);
+		s.readNextToken();
+		if(s.test(equalToken)) {
+			e.ro = RelOperator.parse(equalToken); // MÅ VI SENDE INN SELVE TOKEN ELLER e.ro.opr = equalTOken ??? CHECK
+		
+		
+		leaveParser("expression");
+		return ws;
+	}
 }
--- src/parser/Negation.java
+++ src/parser/Negation.java
@@ -4,5 +4,26 @@ import scanner.*;
 import static scanner.TokenKind.*;
 
 class Negation extends Factor {
+	Factor fac;
+	
+	public Negation(int lNum) {
+		super(lNum);
+	}
+	
+	@Override
+	public String identify() {
+		return "<negation> on line " + lineNum;
+	}
+	
+	public static Negation parse(Scanner s) {
+		enterParser("negation");
+		
+		Negation ws = new Negation(s.curLineNum());
+		
+		s.skip(notToken);
+		ws.fac = Factor.parse(s);
+		leaveParser("negation");
+		return ws;
+	}
 	
 }
